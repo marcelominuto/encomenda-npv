b61694738fcdbcce77d78759bb15ef3b411a62eb
Marcelo <marcelominuto.codes@gmail.com>
2023-04-25 05:28:41 -0300

    Corrigido calculo do redirecionamento, alterado a ordem entre PIX e Parcelado, Adicionado webhook para salvar as cotações no discord.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 const url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+const wbhook = "https://discord.com/api/webhooks/1100328815143686184/wmZKIoxxPGue_11gGh_lP7z-aCWx-tWP0AfmNmrwNAkdJXuCKjsxwYmYKqFTyD8v4mIz"
 
 const formRef = document.querySelector("form");
 
@@ -17,7 +18,15 @@ const stockShippingUSD = 14.95;
 const taxGoat = 0.075;
 const goatShippingUSD = 14.50;
 
-const redirectShippingUSD = 50;
+//Conferir IOF
+const iofCard = 0.04;
+
+//const redirectShippingUSD = 50;
+const usPyShipping = 27.50;
+const pyBrShipping = 0.2; //20% do valor da ask
+const redirectShippingUSD = (ask) =>{
+    return (ask * pyBrShipping) + usPyShipping
+}
 
 let priceUSD;
 
@@ -49,7 +58,7 @@ formRef.addEventListener("submit", (event) => {
     const salesTax = stockPrice * salesTaxPercentage;
     const procFee = stockPrice * stockProcFeePercentage;
 
-    const usStockPrice = stockPrice + salesTax + procFee + redirectShippingUSD + stockShippingUSD
+    const usStockPrice = stockPrice + salesTax + procFee + stockShippingUSD + redirectShippingUSD(stockPrice);
     const brStockPrice = usStockPrice * priceUSD;
 
     const finalStockPrice = (brStockPrice * (marginProfit/100)) + brStockPrice
@@ -67,7 +76,7 @@ formRef.addEventListener("submit", (event) => {
     const goatPrice = parseFloat(goatPriceInput.value);
 
     const goatTax = goatPrice * taxGoat;
-    const usGoatPrice = goatPrice + goatTax + goatShippingUSD + redirectShippingUSD
+    const usGoatPrice = goatPrice + goatTax + goatShippingUSD + redirectShippingUSD(goatPrice);
     const brGoatPrice = usGoatPrice * priceUSD;
 
     const pixGoatPrice = (brGoatPrice * (marginProfit/100) + brGoatPrice);
@@ -96,28 +105,43 @@ formRef.addEventListener("submit", (event) => {
     totalGoatRef.innerHTML = "Valor Total: <strong>R$" + brGoatPrice.toFixed(2)
     lucroGoatRef.innerHTML = "Lucro: <strong style='color: green'>R$" + lucroGoat
 
+    // Cria msg do webhook
+    const orderMsg = createMsg();
+    addField(orderMsg, "Cliente:", nomeCliente)
+    addField(orderMsg, "Modelo:", sneakerModel)
+    addField(orderMsg, "Tamanho:", sneakerSize)
+    addField(orderMsg, "Frete(BRL):", shipping)
+    addField(orderMsg, "Margem em %:", marginProfit)
+    addField(orderMsg, "Droper:", "Preço em BRL: R$" + droperPrice + "\nValor Total: R$" + droperPriceShipping.toFixed(2) + "\nLucro: R$" + lucroDroper)
+
     if(pixGoatPrice > finalStockPrice){
         // Gera a mensagem para StockX
         messageRef.innerHTML = nomeCliente + ", segue a cotação para encomendar " + sneakerModel + " tamanho " + sneakerSize 
-        + "<br><br>TRAZENDO DE FORA<br><br>PIX: R$" + roundNum(finalStockPrice) 
-        + "<br>Parcelado: R$" + roundNum(finalStockPriceParcelado) +
-        "<br><br>BRASIL<br><br>PIX: R$" + roundNum(pixDroperPrice) + "<br>Parcelado: R$" + roundNum(parceladoDroperPrice)
+        + "<br><br>TRAZENDO DE FORA<br><br>Parcelado: R$" + roundNum(finalStockPriceParcelado) 
+        + "<br>PIX: R$" + roundNum(finalStockPrice) +
+        "<br><br>BRASIL<br><br>Parcelado: R$" + roundNum(parceladoDroperPrice) + "<br>PIX: R$" + roundNum(pixDroperPrice)
         stockRef.style.border = "solid green 0.15rem"
-        goatRef.style.border = "none" 
+        goatRef.style.border = "none"
+
+        addField(orderMsg, ":white_check_mark: StockX:", "Preço em USD: $" + stockPrice + "\nValor Total: R$" + brStockPrice.toFixed(2) + "\nLucro: R$" + lucroStock)
+        addField(orderMsg, "Goat:", "Preço em USD: $" + goatPrice + "\nValor Total: R$" + brGoatPrice.toFixed(2) + "\nLucro: R$" + lucroGoat) 
     } else {
         // Gera a mensagem para Goat
         messageRef.innerHTML = nomeCliente + ", segue a cotação para encomendar " + sneakerModel + " tamanho " + sneakerSize 
-        + "<br><br>TRAZENDO DE FORA<br><br>PIX: R$" + roundNum(pixGoatPrice) 
-        + "<br>Parcelado: R$" + roundNum(parceladoGoatPrice) +
-        "<br><br>BRASIL<br><br>PIX: R$" + roundNum(pixDroperPrice) + "<br>Parcelado: R$" + roundNum(parceladoDroperPrice)
+        + "<br><br>TRAZENDO DE FORA<br><br>Parcelado: R$" + roundNum(parceladoGoatPrice) 
+        + "<br>PIX: R$" + roundNum(pixGoatPrice) +
+        "<br><br>BRASIL<br><br>Parcelado: R$" + roundNum(parceladoDroperPrice) + "<br>PIX: R$" + roundNum(pixDroperPrice)
         goatRef.style.border = "solid green 0.15rem"
-        stockRef.style.border = "none" 
+        stockRef.style.border = "none"
+
+        addField(orderMsg, "StockX:", "Preço em USD: $" + stockPrice + "\nValor Total: R$" + brStockPrice.toFixed(2) + "\nLucro: R$" + lucroStock)
+        addField(orderMsg, ":white_check_mark: Goat:", "Preço em USD: $" + goatPrice + "\nValor Total: R$" + brGoatPrice.toFixed(2) + "\nLucro: R$" + lucroGoat) 
     }
 
-
-
     messageBoxRef.style.display = "flex";
-        
+    
+    sendWebhook(orderMsg);
+
 })
 
 copyRef.addEventListener("click", () => {
@@ -144,7 +168,7 @@ function toast(message){
 function roundNum(x) {
     x.toFixed(1)
     return Math.round(x / 10) * 10;
-  }
+}
 
 getUSD()
 const usdRef = document.getElementById("usd");
@@ -154,3 +178,38 @@ setTimeout(() =>{
     loadingIcon.style.display = "none";
     usdRef.innerHTML = "Valor Dolár: <strong style='color: green'>R$" + priceUSD.toFixed(2); 
 }, 1000)
+
+function createMsg(){
+    return {
+        "embeds": [
+          {
+            "title": ":package: ENCOMENDA COTADA!",
+            "color": 2463422,
+            "description": "",
+            "timestamp": new Date(),
+            "thumbnail": {
+              "url": "https://marcelominuto.github.io/encomenda-npv/images/logo%20npv%20transparente.png"
+            },
+            "footer": {
+              "text": "NPV Sneakers",
+              "icon_url": "https://marcelominuto.github.io/encomenda-npv/images/logo%20npv%20transparente.png"
+            },
+            "fields": []
+          }
+        ],
+    }
+}
+
+function addField(msg, name, value){
+    msg.embeds[0].fields.push({
+        name, 
+        value
+    })
+}
+
+function sendWebhook(msg){
+    fetch(wbhook + "?wait=true", 
+    {"method":"POST", "headers": {"content-type": "application/json"},
+    "body": JSON.stringify(msg)})
+    .then(a=>a.json()).then(console.log)
+}
